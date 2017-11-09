b8224ac224f6f3290807fe5825d6a7c45cbec5a5
Jan Havluj <jan@havluj.eu>
2017-11-09 14:04:48 +0100

    Adds run.[sh|bat] script to run the app seamlessly. Also pom.xml modifications, so the app packages into a jar and copies run scripts into the target dir.

---
+++ data/tagger/src/bin/run.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set dataLoc=%1
+
+:: Find Java binary
+@set JAVA_BIN=%~dp0..\..\..\jre_x64\bin\java.exe
+@if not exist "%JAVA_BIN%" >NUL 2>&1 set JAVA_BIN=%JAVA_HOME%\bin\java.exe
+@if not exist "%JAVA_BIN%" >NUL 2>&1 set JAVA_BIN=%JAVA_HOME%\jre\bin\java.exe
+@if not exist "%JAVA_BIN%" >NUL 2>&1 ( set JAVA_BIN=java.exe & where /q java.exe )
+@if errorlevel 1 echo No Java found. Please put Java bin directory into your PATH environment or set JAVA_HOME environment variable to valid Java installation. && exit /b 1
+
+:: Run the app
+@"%JAVA_BIN%" -jar target\tagger-1.0-SNAPSHOT.jar --data.location=%dataLoc%
