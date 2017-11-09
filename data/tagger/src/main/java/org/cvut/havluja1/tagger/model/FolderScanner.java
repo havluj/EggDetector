7e3ee1108a1a3bcd2c7a8385506fff7d2c2032ff
Jan Havluj <jan@havluj.eu>
2017-11-09 13:14:18 +0100

    Folder scanner further improvements

--- data/tagger/src/main/java/org/cvut/havluja1/tagger/model/FolderScanner.java
+++ data/tagger/src/main/java/org/cvut/havluja1/tagger/model/FolderScanner.java
@@ -3,6 +3,9 @@ package org.cvut.havluja1.tagger.model;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.FilenameUtils;
 
 public class FolderScanner {
     public static List<String> scanFolder(String location) {
@@ -12,17 +15,30 @@ public class FolderScanner {
             return new ArrayList<>();
         }
 
+        List<String> result = new ArrayList<>();
 
-        for (File dir : locFile.listFiles((file, name) -> {
-            // is a dir
-            // is in a correct format
+        // example folder name: 20160430_073822_526_D
+        final Pattern pattern = Pattern.compile("\\d{8}_\\d{6}_\\d{3}_D");
+        final File[] fileList = locFile.listFiles((File file, String name) -> {
+            // needs to be a dir in a correct format
+            if (!file.isDirectory() && !pattern.matcher(name).matches()) {
+                return false;
+            }
+
+            // if already tagged
+            // todo check the db
+
             // contains any pictures
+            if (file.list((f, n) -> FilenameUtils.getExtension(n).equals("png")).length <= 0) {
+                return false;
+            }
+
             return true;
-
-            // if already tagged,
-        })) {
-
+        });
+        for (File dir : fileList) {
+            result.add(dir.getAbsolutePath());
         }
-        return new ArrayList<>();
+
+        return result;
     }
 }
