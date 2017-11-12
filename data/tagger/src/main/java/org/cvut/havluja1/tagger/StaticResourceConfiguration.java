a76239e5682ba678a8bea988a636e7cd26e3543c
Jan Havluj <jan@havluj.eu>
2017-11-12 18:30:40 +0100

    resource fix

--- data/tagger/src/main/java/org/cvut/havluja1/tagger/StaticResourceConfiguration.java
+++ data/tagger/src/main/java/org/cvut/havluja1/tagger/StaticResourceConfiguration.java
@@ -1,14 +1,19 @@
 package org.cvut.havluja1.tagger;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
+
+    @Value("${data.location}")
+    private String dataLocation;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         super.addResourceHandlers(registry);
-        registry.addResourceHandler("/**").addResourceLocations("file:///C:/school/bakalarka/data");
+        registry.addResourceHandler("/**").addResourceLocations("file:///" + dataLocation);
     }
 }
