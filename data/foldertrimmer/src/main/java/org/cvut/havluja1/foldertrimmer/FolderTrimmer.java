f287acfc271b6e3c319628d9a0a70117e5ae25b5
Jan Havluj <jan@havluj.eu>
2017-12-08 19:08:01 +0100

    new project: folder trimmer

--- data/foldertrimmer/src/main/java/org/cvut/havluja1/foldertrimmer/FolderTrimmer.java
+++ data/foldertrimmer/src/main/java/org/cvut/havluja1/foldertrimmer/FolderTrimmer.java
@@ -1,7 +1,45 @@
 package org.cvut.havluja1.foldertrimmer;
 
-public class FolderTrimmer {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+
+public class FolderTrimmer {
+    public static void main(String[] args) throws IOException {
+        File rootDir = new File(args[0]);
+
+        if (!rootDir.exists() || rootDir.isDirectory()) {
+            throw new IllegalArgumentException("root dir does not exist");
+        }
+
+        List<String> emptyDirs = findAndDeleteEmptyDirs(rootDir);
+        emptyDirs.forEach(x -> System.out.println(x));
+    }
+
+    private static List<String> findAndDeleteEmptyDirs(File dir) {
+        List<String> deletedFolders = new ArrayList<String>();
+        boolean shouldBeDeleted = false;
+
+        File[] toBeProcessed = dir.listFiles();
+        // list everything
+        // if file and is not xml, txt or png return, if it is, tag this folder not to be deleted
+        // if dir -> return and tag this folder not to be deleted
+
+        if(shouldBeDeleted) {
+            try {
+                FileUtils.deleteDirectory(dir);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            // foreach on all results
+            // if dir -> recursive call
+            // if file -> delete
+        }
+
+        return deletedFolders;
     }
 }
