56b83b94fb052bd20dafb852a9a250c665eafe83
Jan Havluj <jan@havluj.eu>
2017-12-27 00:51:46 +0100

    added basic test with gui

---
+++ eggdetector/src/test/java/org/cvut/havluja1/eggdetector/BasicTest.java
@@ -0,0 +1,19 @@
+package org.cvut.havluja1.eggdetector;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class BasicTest {
+
+    private static final String DIR = "/run/media/jan/data/bachelor_thesis_data/data/egg_count_images/20160421_191543_725_D/";
+
+    @Test
+    public void test() {
+        EggDetector eggDetector = new EggDetector();
+        eggDetector.setDebugMode(true);
+        SequenceClassifier seq = eggDetector.evaluate(new File(DIR));
+        System.out.println("Final result: " + seq.getFinalCount());
+        System.out.println("Individual scores: " + seq.getIndividualCounts());
+    }
+}
