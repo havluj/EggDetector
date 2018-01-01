218a938a5cf365817f6bdb03254922e5c899df2e
Jan Havluj <jan@havluj.eu>
2018-01-01 17:11:28 +0100

    nastroje - intro a hromadny download

--- tools/download.sh
+++ tools/download.sh
@@ -3,9 +3,4 @@
 DIRECTORY=data
 URL=http://athena.pef.czu.cz/ptacionline/
 
-if [ -d "$DIRECTORY" ]; then
-  rm -rf "$DIRECTORY"
-fi
-
-mkdir "$DIRECTORY"
 wget -o log.txt -nv --show-progress -c -P "$DIRECTORY" -r -np -nH --cut-dirs=1 -R index.html "$URL"
